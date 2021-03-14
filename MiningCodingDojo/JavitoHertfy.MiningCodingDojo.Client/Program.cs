5d3dc9b0495ea92120972c09686ea447ea69bbf6
Javier Hertfelder <javier@fxstreet.com>
2021-03-14 10:17:47 +0100

    Optimized miner!

--- MiningCodingDojo/JavitoHertfy.MiningCodingDojo.Client/Program.cs
+++ MiningCodingDojo/JavitoHertfy.MiningCodingDojo.Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Threading;
@@ -11,50 +12,41 @@ namespace JavitoHertfy.MiningCodingDojo.Client
     public class Program
     {
         private static string minerId = "0b361510-6f7b-43fb-99eb-f7c4b188f10a";
-
+        private static int totalQuantity = 0;
         static void Main(string[] args)
         {
-            var totalQuantity = 0;
+          
             var shouldContinue = true;
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
             var client = new RestClient("http://mining.azurewebsites.net/");
             SignUp(client);           
+
             do
             {
                 var quantity = Mine(client);
-                if(quantity > 0)
+                if (quantity > 0)
                 {
                     Save(client, quantity);
                     totalQuantity += quantity;
-                }                    
+                }
                 if (totalQuantity > 500)
                 {
                     stopwatch.Stop();
                     shouldContinue = false;
-                }                    
+                    Console.WriteLine($"It took {stopwatch.ElapsedMilliseconds} to mine 500");
+                }
 
             } while (shouldContinue);
 
-            Console.WriteLine($"It took {stopwatch.ElapsedMilliseconds} to mine 1000");
         }       
         private static bool SignUp(RestClient client)
         {
             var requestSignUp = new RestRequest($"api/miner/signup/{minerId}", DataFormat.Json);
             var responseSignUp = default(IRestResponse);
-
             do
             {
-                responseSignUp = client.Put(requestSignUp);
-                if (responseSignUp.IsSuccessful)
-                {
-                    if (bool.Parse(responseSignUp.Content))
-                        Console.WriteLine("Now I am in the mine!");
-                }
-                else
-                {
-                    Console.WriteLine("Something went wrong entering in the mine");
-                }
+                responseSignUp = client.Put(requestSignUp);                
 
             } while (responseSignUp.IsSuccessful);
 
@@ -63,17 +55,14 @@ namespace JavitoHertfy.MiningCodingDojo.Client
         }
         private static int Mine(RestClient client)
         {
-            var requestMine = new RestRequest($"/api/GoldMine/Dig/0b361510-6f7b-43fb-99eb-f7c4b188f10a", DataFormat.Json);          
-
+            var requestMine = new RestRequest($"/api/GoldMine/Dig/0b361510-6f7b-43fb-99eb-f7c4b188f10a", DataFormat.Json);    
             var responseMine = default(IRestResponse);
             do
             {
                 responseMine = client.Put(requestMine);
                 if (responseMine.IsSuccessful)
-                {
-                    var gold = int.Parse(responseMine.Content);
-                    Console.WriteLine($"Uray! I mined {gold}");
-                    return gold;
+                {                                     
+                    return int.Parse(responseMine.Content); ;
                 }
                 else
                     if(responseMine.StatusCode == System.Net.HttpStatusCode.Unauthorized)
@@ -87,16 +76,12 @@ namespace JavitoHertfy.MiningCodingDojo.Client
         private static int Save(RestClient client, int quantity)
         {
             var requestSave = new RestRequest($"/api/Miner/SaveGold/0b361510-6f7b-43fb-99eb-f7c4b188f10a?quantity={quantity}", DataFormat.Json);
-            
-            requestSave.AddQueryParameter("quantity", quantity.ToString());
-
             var responseSave = default(IRestResponse);
             do
             {
                 responseSave = client.Put(requestSave);
                 if (responseSave.IsSuccessful)
-                {
-                    Console.WriteLine($"Now {quantity} gold is in my pocket!!");
+                {                    
                     return quantity;
                 }
                 else
@@ -113,17 +98,6 @@ namespace JavitoHertfy.MiningCodingDojo.Client
     public class Miner
     {
         public string Name { get; set; }
-    }
-
-    public class MinerResponse
-    {
-
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public int Quantity { get; set; }
-        public int Handicap { get; set; }
-        public bool IsLogged { get; set; }
-    }
-     
+    } 
 
 }
